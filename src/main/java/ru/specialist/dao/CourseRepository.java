fbbd2c51d106e8b402180f44068f54d9adbe4b1f
d.bala <dmitriybala@gmail.com>
2020-12-25 21:43:43 +0300

    Spring MVC lab

---
+++ src/main/java/ru/specialist/dao/CourseRepository.java
@@ -0,0 +1,6 @@
+package ru.specialist.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Integer> {
+}
---
+++ src/main/java/ru/specialist/dao/CourseServiceImpl.java
@@ -0,0 +1,42 @@
+package ru.specialist.dao;
+
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
+@Service("courseService")
+public class CourseServiceImpl implements CourseService {
+
+    private CourseRepository courseRepository;
+
+    @Autowired
+    public void setCourseRepository(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Course> findAll() {
+        return Lists.newArrayList(courseRepository.findAll());
+    }
+
+    @Transactional(readOnly = true)
+    public Course findById(int id) {
+        return courseRepository.findOne(id);
+    }
+
+    public Course save(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public void delete(int id) {
+        courseRepository.delete(id);
+    }
+
+
+}
